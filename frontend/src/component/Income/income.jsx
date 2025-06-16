7a415c492a57714e739bede6218f52e01e5968d4
juniorashu <87563165+juniorashu@users.noreply.github.com>
2025-06-16 18:46:28 +0100

    Update income.jsx
    income.css correction

--- frontend/src/component/Income/income.jsx
+++ frontend/src/component/Income/income.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { getAuth } from 'firebase/auth';
-import './Income.css';
+import './income.css';
 import AddSalaryForm from './addIncomeform';
 
 function Income() {
