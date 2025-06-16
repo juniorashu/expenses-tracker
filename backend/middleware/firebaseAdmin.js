f68bf0338f709546562ab979b4a7f81b411a438f
juniorashu <87563165+juniorashu@users.noreply.github.com>
2025-06-16 17:05:36 +0100

    Create firebaseAdmin.js
    creating a firebaseAdmin file because it was not push

---
+++ backend/middleware/firebaseAdmin.js
@@ -0,0 +1,11 @@
+// firebaseAdmin.js
+import admin from "firebase-admin";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const serviceAccount = require("../config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
