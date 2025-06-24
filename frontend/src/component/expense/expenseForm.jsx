29d54fc12a26e225481157f8f84fc8c0766a5f7e
juniorashu <87563165+juniorashu@users.noreply.github.com>
2025-06-24 18:09:13 +0100

    Update expenseForm.jsx

--- frontend/src/component/expense/expenseForm.jsx
+++ frontend/src/component/expense/expenseForm.jsx
@@ -25,7 +25,7 @@ const ExpenseForm = ({ onAddExpense }) => {
       const token = await getIdToken(user); // ✅ Get ID token
 
       const res = await axios.post(
-        'http://localhost:5000/api/expenses',
+        'https://expenses-tracker-jn6x.onrender.com/api/expenses',
         newExpense,
         {
           headers: {
