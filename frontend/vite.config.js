8d97eecca5542d943ef1423bf80323b0116da82c
juniorashu <87563165+juniorashu@users.noreply.github.com>
2025-06-24 18:11:57 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://expenses-tracker-jn6x.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') // ✅ fixed
       }
