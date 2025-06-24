325827dc70170808a07ea6ba0834817a25d6a653
juniorashu <87563165+juniorashu@users.noreply.github.com>
2025-06-24 18:06:05 +0100

    Update income.jsx

--- frontend/src/component/Income/income.jsx
+++ frontend/src/component/Income/income.jsx
@@ -15,7 +15,7 @@ function Income() {
     if (!user) return;
 
     const token = await user.getIdToken();
-    axios.get('http://localhost:5000/api/incomes', {
+    axios.get('https://expenses-tracker-jn6x.onrender.com/api/incomes', {
       headers: {
         Authorization: `Bearer ${token}`
       }
@@ -39,7 +39,7 @@ function Income() {
     if (!user) return;
 
     const token = await user.getIdToken();
-    axios.delete(`http://localhost:5000/api/incomes/${id}`, {
+    axios.delete(`https://expenses-tracker-jn6x.onrender.com/api/incomes/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
